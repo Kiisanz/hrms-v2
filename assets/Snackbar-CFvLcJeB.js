7c27fa54595976acf6bd8bc03b588e2f994f22ab
aras-dev <arassoftwaredev@gmail.com>
2025-01-22 14:24:30 +0700

    update fitur calendar

---
+++ assets/Snackbar-CFvLcJeB.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-XHbx4tvh.js";const s=({text:t,show:e})=>a.jsx("div",{className:`fixed ${e?"animate-fade-in":"animate-fade-out"} text-xs bottom-5 w-[90%] text-white translate-x-1/2 right-1/2 bg-slate-800 px-3 py-2 rounded-full`,children:t});export{s as S};
