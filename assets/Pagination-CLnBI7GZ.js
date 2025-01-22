27da2c4c5890b4a28715444eb0788374fc880f33
aras-dev <arassoftwaredev@gmail.com>
2025-01-22 10:43:44 +0700

    update bug url

---
+++ assets/Pagination-CLnBI7GZ.js
@@ -0,0 +1,6 @@
+import{c as a,j as t}from"./index-KZjeQIkU.js";import{I as d}from"./IconChevronRight-C5IwC07n.js";/**
+ * @license @tabler/icons-react v3.22.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var r=a("outline","chevron-left","IconChevronLeft",[["path",{d:"M15 6l-6 6l6 6",key:"svg-0"}]]);const p=({currentPage:e,totalPages:s,onPageChange:n})=>{const o=()=>{e>1&&n(e-1)},i=()=>{e<s&&n(e+1)};return t.jsxs("div",{className:"flex justify-center items-center gap-4 bg-white rounded-b-lg py-1 ",children:[t.jsx("button",{onClick:o,disabled:e===1,className:"text-slate-800 disabled:text-gray-400 px-3 py-1 hover:bg-gray-100 disabled:hover:bg-transparent rounded-md transition",children:t.jsx(r,{})}),t.jsxs("span",{className:"text-sm font-medium text-gray-700 px-4 py-1",children:["Page ",e," of ",s]}),t.jsx("button",{onClick:i,disabled:e===s,className:"text-slate-800 disabled:text-gray-400 px-3 py-1 hover:bg-gray-100 disabled:hover:bg-transparent rounded-md transition",children:t.jsx(d,{})})]})};export{p as P};
