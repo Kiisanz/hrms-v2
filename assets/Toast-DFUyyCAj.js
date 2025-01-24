c6ce80e975327767edeafb3a9cd2d7ba248d5033
aras-dev <arassoftwaredev@gmail.com>
2025-01-24 14:49:45 +0700

    update

---
+++ assets/Toast-DFUyyCAj.js
@@ -0,0 +1,11 @@
+import{c as s,j as e}from"./index-DGZRTPoJ.js";import{I as r,a as i}from"./IconCircleXFilled-CrLcQVUO.js";/**
+ * @license @tabler/icons-react v3.22.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var n=s("outline","alert-triangle","IconAlertTriangle",[["path",{d:"M12 9v4",key:"svg-0"}],["path",{d:"M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z",key:"svg-1"}],["path",{d:"M12 16h.01",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.22.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o=s("outline","x","IconX",[["path",{d:"M18 6l-12 12",key:"svg-0"}],["path",{d:"M6 6l12 12",key:"svg-1"}]]);const x=({text:l,type:t,onClick:a})=>e.jsx("div",{children:e.jsx("div",{className:"absolute bottom-5 right-5 bg-slate-800 shadow-lg rounded-xl z-30 ",id:"notification",children:e.jsxs("div",{className:"flex gap-3 w-full items-center p-4",children:[e.jsx("div",{className:`rounded-full p-1 bg-white border-2 border-white ${t==="warn"?"text-yellow-500":t==="error"?"text-red-500":"text-green-500"} text-xl`,children:t==="warn"?e.jsx(n,{}):t==="error"?e.jsx(r,{}):e.jsx(i,{})}),e.jsxs("div",{className:"flex justify-between items-center gap-6",children:[e.jsx("h1",{className:"font-semibold text-slate-100",children:l}),e.jsx("button",{onClick:a,type:"button",children:e.jsx(o,{className:"text-white"})})]})]})})});export{o as I,x as T};
