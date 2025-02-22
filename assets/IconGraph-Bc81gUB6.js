f8915e8cdfa8f7fb269650666e0aaaa6f4894cf9
aras-dev <arassoftwaredev@gmail.com>
2025-02-22 14:28:20 +0700

    update lagi aja njir

---
+++ assets/IconGraph-Bc81gUB6.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-URPNpmk_.js";/**
+ * @license @tabler/icons-react v3.22.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var l=a("outline","alarm","IconAlarm",[["path",{d:"M12 13m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0",key:"svg-0"}],["path",{d:"M12 10l0 3l2 0",key:"svg-1"}],["path",{d:"M7 4l-2.75 2",key:"svg-2"}],["path",{d:"M17 4l2.75 2",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.22.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var t=a("outline","graph","IconGraph",[["path",{d:"M4 18v-12a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z",key:"svg-0"}],["path",{d:"M7 14l3 -3l2 2l3 -3l2 2",key:"svg-1"}]]);export{l as I,t as a};
